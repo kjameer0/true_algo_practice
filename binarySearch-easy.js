b00f3b6024456483782bd7a3ff5c51d942d3f768
Khalid Jameer <kjameer0@gmail.com>
2022-05-27 21:30:30 -0400

    binary search complete

---
+++ binarySearch-easy.js
@@ -0,0 +1,11 @@
+var search = function (nums, target) {
+  let [left, right] = [0, nums.length - 1];
+  let mid;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) left = mid + 1;
+    else right = mid - 1;
+  }
+  return -1;
+};
