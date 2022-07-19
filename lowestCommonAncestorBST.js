1b865fabbbb5ecf9deac65d422100148554889cd
Khalid Jameer <kjameer0@gmail.com>
2022-07-19 18:19:35 -0400

    lowest common ancestor BST

--- lowestCommonAncestorBST.js
+++ lowestCommonAncestorBST.js
@@ -10,3 +10,19 @@ var lowestCommonAncestor = function (root, p, q) {
 //another common parent, because a BST can only have a greater
 //and less than child, so if theyre both greater, then something
 //else must be above them as a parent
+var lowestCommonAncestor2 = function (root, p, q) {
+  let res;
+  function trav(root, p, q) {
+    if (!root) return;
+    else {
+      if (p.val < root.val && q.val < root.val) trav(root.left, p, q);
+      else if (p.val > root.val && q.val > root.val) trav(root.right, p, q);
+      else {
+        res = root;
+        return;
+      }
+    }
+  }
+  trav(root, p, q);
+  return res;
+};
