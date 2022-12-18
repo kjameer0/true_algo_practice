3c6a5dfec5b6b4000364c45ccf436f51aeeff927
Khalid Jameer <kjameer0@gmail.com>
2022-12-17 21:16:21 -0500

    count triple xors

--- experimentation/countTriplestThatCanFormTwpArraysOfEqualXOR.js
+++ experimentation/countTriplestThatCanFormTwpArraysOfEqualXOR.js
@@ -1,11 +1,25 @@
+/*Given an array of integers arr.
+
+We want to select three indices i, j and k where (0 <= i < j <= k < arr.length).
+
+Let's define a and b as follows:
+
+a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+Note that ^ denotes the bitwise-xor operation.
+
+Return the number of triplets (i, j and k) Where a == b.
+
+*/
 var countTriplets = function (arr) {
   if (arr.length < 2) return 0;
   var xors = [arr[0]];
-  for (var i = 1; i < arr.length; i++) xors[i] = xors[i - 1] ^ arr[i];
+  for (var l = 1; l < arr.length; l++) xors[l] = xors[l - 1] ^ arr[l];
+  console.log(xors);
   var dp = [0, arr[0] === arr[1]];
   for (var idx = 2; idx < arr.length; idx++) {
-    var k = idx;
-    var rightXor = 0;
+    var k = idx,
+      rightXor = 0;
     dp[idx] = dp[idx - 1];
     for (var j = k; j > 0; j--) {
       rightXor ^= arr[j];
@@ -18,5 +32,24 @@ var countTriplets = function (arr) {
       }
     }
   }
+  console.log(dp);
   return dp[arr.length - 1];
 };
+
+//console.log(countTriplets([2, 3, 1, 6, 7]));
+function countBits(n) {
+  let ans = new Array(n + 1).fill(0);
+  let x = 0,
+    b = 1;
+  // # [0, b) is calculated
+  while (b <= n) {
+    // # generate [b, 2b) or [b, n) from [0, b)
+    while (x < b && x + b <= n) {
+      ans[x + b] = ans[x] + 1;
+      x += 1;
+    }
+    x = 0; //# reset x
+    b = b << 1; //# b = 2b
+  }
+  return ans;
+}
